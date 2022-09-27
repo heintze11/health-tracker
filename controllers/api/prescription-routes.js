f13a8c039fcb89f16186b71cd0d46c5c2c151e85
thaling <thaling@verizon.net>
2022-09-26 19:35:30 -0600

    routes basic set up

---
+++ controllers/api/prescription-routes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const { response } = require('express');
+//const { Prescription } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+
+
+
+
+
+
+module.exports = router;
---
+++ models/appointment.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const { response } = require('express');
+//const { Appointment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+
+
+
+
+
+
+module.exports = router;
