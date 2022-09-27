ca4f1946f740dcc9013608866a3417b4725939b8
thaling <thaling@verizon.net>
2022-09-26 20:18:45 -0600

    user.js routes, auth file

---
+++ controllers/api/appointment-routes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const { response } = require('express');
+const { Appointments } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+
+
+
+
+
+
+module.exports = router;
--- controllers/api/doctor-routes.js
+++ controllers/api/doctor-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { response } = require('express');
-//const { Doctor } = require('../../models');
+const { Doctors } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 
