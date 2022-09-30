21b5f1a45a915f56eec45d2671fdf66ff87a4069
AFaidley <alviva.faidley@gmail.com>
2022-09-29 21:20:49 -0600

    Worked on handlebars and created new js files

---
+++ public/js/doctor.js
@@ -0,0 +1,31 @@
+const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/users/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to delete. Please try again');
+      }
+    }
+  };
+
+  const createButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/users/${id}`, {
+        method: 'POST',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to create. Please try again');
+      }
+    }
+  };
