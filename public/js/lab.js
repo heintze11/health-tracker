19c5e946187513c17fc97d37ff19366cc176d933
thaling <thaling@verizon.net>
2022-10-01 15:13:20 -0600

    lab delete function working

--- public/js/lab.js
+++ public/js/lab.js
@@ -1,5 +1,7 @@
 const labForm = document.querySelector(".lab-form");
+const labCard = document.querySelector(".lab-card");
 
+// Create lab
 const createButtonHandler = async (event) => {
   event.preventDefault();
   const name = document.querySelector('input[name="name"]').value;
@@ -22,8 +24,33 @@ const createButtonHandler = async (event) => {
   }
 };
 
+// Delete lab
+const handleLabDelete = (e) => {
+  e.stopPropagation();
+
+  const lab = e.target;
+
+  const labId = JSON.parse(lab.getAttribute("id"));
+
+  deleteLab(labId);
+};
+
+const deleteLab = async (id) => {
+  const response = await fetch(`/api/lab/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (response.ok) {
+    document.location.reload("/api/lab");
+  } else {
+    alert(response.statusText);
+  }
+};
+
 document
   .querySelector(".lab-form")
   .addEventListener("submit", createButtonHandler);
 
-
+document.querySelector(".lab-card").addEventListener("click", handleLabDelete);
